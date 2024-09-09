dbbbd30435c70f290afe96da148147428b081621
Satru <sideichowde@gmail.com>
2024-09-09 01:31:28 -0500

    Update .gitignore

--- config/conex.php
+++ config/conex.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "Fernandaedward17";
 $dbname = "usc";
 
 $mysqli = new mysqli($servername, $username, $password, $dbname);
